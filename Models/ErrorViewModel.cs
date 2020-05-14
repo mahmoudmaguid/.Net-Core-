a3d0005517cc507185024511996e3c6c0f23931d
Mahmoud Maguid <mahmoud.maguid@yahoo.ca>
2020-05-14 17:58:45 -0600

    v1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NET_CORE_Assignemnt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
